5a15f9a6955f8915a0d454370ac2c7e21231f4e7
mrayusha8-hue <mrayusha8@gmail.com>
2026-03-01 18:45:27 +0545

    Added basic SELECT queries practice

---
+++ basic_select.sql
@@ -0,0 +1,25 @@
+SELECT * 
+FROM  employee_demographics;
+
+SELECT * 
+FROM parks_and_recreation.employee_demographics;
+
+SELECT employee_id
+FROM  employee_demographics;
+
+SELECT first_name,age,age+10
+FROM  employee_demographics;  
+
+SELECT * 
+FROM employee_demographics;
+
+SELECT first_name ,last_name,birth_date
+ from employee_demographics;
+
+SELECT distinct gender from employee_demographics;
+
+
+
+
+
+
